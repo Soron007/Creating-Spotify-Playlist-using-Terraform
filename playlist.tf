4dfa18345edbcb118d170c734aa2dffa6a906c42
root <root@DESKTOP-FM1MCIB.>
2025-02-19 10:15:41 +0000

    added another playlist

--- playlist.tf
+++ playlist.tf
@@ -1,4 +1,21 @@
-resource "spotify_playlist" "Bollywood" {
-  name = "Bollywood"
-  tracks = ["3ZOEytgrvLwQaqXreDs2Jx"]
+resource "spotify_playlist" "Peace" {
+  name        = "Peace"
+  description = "Peace is all we need"
+  public      = false
+
+  tracks = [
+    data.spotify_track.overkill.id,
+    data.spotify_track.blackwater.id,
+    data.spotify_track.snowblind.id,
+  ]
+}
+
+data "spotify_track" "overkill" {
+  url = "https://open.spotify.com/track/4XdaaDFE881SlIaz31pTAG"
+}
+data "spotify_track" "blackwater" {
+  url = "https://open.spotify.com/track/4lE6N1E0L8CssgKEUCgdbA"
+}
+data "spotify_track" "snowblind" {
+  url = "https://open.spotify.com/track/7FCG2wIYG1XvGRUMACC2cD"
 }
